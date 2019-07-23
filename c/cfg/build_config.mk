a8e90e8087f5beb6c0a30c45128f04efe794b1a2
ronaldo <ronaldo@cheesetea.com>
2019-07-24 00:21:55 +0200

    Renamed game to RPG instead of AGC01

--- c/cfg/build_config.mk
+++ c/cfg/build_config.mk
@@ -48,7 +48,7 @@
 
 # Name of the project (without spaces, as it will be used as filename)
 #   and Z80 memory location where code will start in the generated binary
-PROJNAME   := agc01
+PROJNAME   := rpg
 Z80CODELOC := 0x4000
 
 ##
